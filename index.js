f3a6d99593a691a9ab89675e2aa8ac575fec19d2
Bertók Attila <bertok.attila99@gmail.com>
2021-09-16 20:42:20 +0200

    init

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+
+const app = express();
+const session = require("express-session");
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//app.use(express.static("static"));
+
+app.use(
+	session({
+		secret: "secret",
+	})
+);
+
+app.use((err, req, res, next) => {
+	res.end("Problem...");
+	console.log(err);
+});
+
+require("./route/router")(app);
+
+app.listen(3000, function () {
+	console.log("Hello :3000");
+});
