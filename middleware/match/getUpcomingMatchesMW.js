55ab421d8e3fa5695da21e17c47a4eb270be137d
Bertók Attila <bertok.attila99@gmail.com>
2021-10-29 18:58:25 +0200

    ejs render

--- middleware/match/getUpcomingMatchesMW.js
+++ middleware/match/getUpcomingMatchesMW.js
@@ -1,8 +1,11 @@
+const matches = require('../../mockDatabase/matches');
 /**
  * Gets the upcoming matches from the db and them it on res.locals
  */
 module.exports = () => {
 	return (req, res, next) => {
+		res.locals.matches = matches;
+
 		return next();
 	};
 };
